4f673db5d8f639078912b3b217622b97f89f3e34
lowestprime <cooperbeaman@gmail.com>
2024-07-10 21:13:17 -0700

    Refactor code to split merged_data_final by TOPMed ethnicity

---
+++ Analysis/Bak/CB.SCS_ROC_MLMA_TXT_PREP.R
@@ -0,0 +1,264 @@
+#### GCTA GWAS PREP ####
+
+# Phenotypes:
+# roi_columns <- smri.R5.1.baseline.y2.ROC.filtered %>% 
+#   dplyr::select(starts_with("smri_vol_")) %>% 
+#   colnames()
+
+## Discrete Covariates ##
+# sex, batch, mri_info_deviceserialnumber
+
+## Quantitative Covariates ##
+# interview_age, bigsnpr top 10 PCs, smri_vol_scs_intracranialv (except for smri_vol_scs_wholeb)
+
+## Priorities ##
+# Ethnicity: EUR
+# ROI: smri_vol_scs_wholeb (smri_vol_scs_intracranialv covar not needed)
+
+# Load Packages
+p_load(purrr, readr, FRGEpistasis, dplyr)
+
+# Directories
+base_dir <- '/u/project/lhernand/cobeaman/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis/'
+r5.1_genetics <- '/u/project/lhernand/shared/GenomicDatasets/ABCD_Release_5.1/core/genetics'
+image_dir <- paste0(base_dir, 'Images/')
+shiny_dir <- paste0(base_dir, 'plots/Shinyapps_plotly_plots/')
+table1_dir <- paste0(shiny_dir, 'Counts.Table')
+table2_dir <- paste0(shiny_dir, 'ROC.Summary.Table')
+gwas_dir <- paste0(base_dir, 'GCTA_GWAS/')
+proc_dir <- paste0(gwas_dir, 'Processed_Data')
+pheno_dir <- file.path(proc_dir, "Phenotypes")
+covar_dir <- file.path(proc_dir, "Covariates")
+anc_pc_dir <- paste0(gwas_dir, 'ANCESTRY_PCS/')
+
+# Define ancestries and sexes
+ethnicities <- c("AFR", "AMR", "EUR")
+sexes <- c("F", "M")
+
+# Set Date
+date <- format(Sys.Date(), "%m%d%Y")
+
+# Load ABCD 5.1 external functions and Sruthi covar file
+source(paste0(base_dir, 'release5_external_functions.R'))
+covariate_file <- "/u/project/lhernand/sganesh/gwas_srs/phenotypes/covar_AllSubj_batch_gender_noNAs_baseline_11665.txt"
+
+# remove non-ROI columns from smri.R5.1.baseline.y2.ROC and reorder columns
+smri.R5.1.baseline.y2.ROC.filtered <- roi_filter(smri.R5.1.baseline.y2.ROC)
+
+# rename pheno cols to GCTA format
+gcta.pheno.scs.vol.roc <- smri.R5.1.baseline.y2.ROC.filtered %>%
+  rename(FID = rel_family_id, IID = src_subject_id)
+
+# Load and preprocess ancestry principal components data by renaming and selecting columns
+ancestry_pcs <- fread(paste0(anc_pc_dir, "ABCD5_all_ancestries_all_individuals_PC20.txt")) %>%
+  rename(IID = V1) %>%
+  dplyr::select(-V2)
+
+# Apply the renaming function to the ancestry_pcs data frame
+ancestry_pcs <- ancestry_pcs %>% rename_with(rename_columns, .cols = starts_with("V"))
+
+# remove non-ROI columns from smri.R5.1.baseline.y2.ROC and reorder columns
+smri.R5.1.baseline.y2.ROC.filtered <- roi_filter(smri.R5.1.baseline.y2.ROC)
+
+# rename pheno cols to GCTA format
+gcta.pheno.scs.vol.roc <- smri.R5.1.baseline.y2.ROC.filtered %>%
+  rename(FID = rel_family_id, IID = src_subject_id)
+
+#### Remove samples with missing data ####
+# Read the files without headers
+covariate_data <- read.table(covariate_file, header = FALSE)
+
+# Assign appropriate column names for phenotype_data and covariate_data
+colnames(covariate_data) <- c("FID", "IID", "batch", "sex")
+
+# Merge gcta.pheno.scs.vol.roc with covariate_data genotyping batch and ancestry_pcs 1-20
+# Filter covariate_data and ancestry_pcs to include only the IIDs present in gcta.pheno.scs.vol.roc
+filtered_covariate_data <- covariate_data %>% semi_join(gcta.pheno.scs.vol.roc, by = "IID")
+filtered_ancestry_pcs <- ancestry_pcs %>% semi_join(gcta.pheno.scs.vol.roc, by = "IID")
+
+# Merge gcta.pheno.scs.vol.roc with filtered_covariate_data and filtered_ancestry_pcs
+merged_data <- gcta.pheno.scs.vol.roc %>%
+  left_join(filtered_covariate_data %>% dplyr::select(IID, batch), by = "IID") %>%
+  left_join(filtered_ancestry_pcs %>% dplyr::select(IID, starts_with("PC")), by = "IID")
+
+# drop any rows from merged_data with NAs (in batch and PCs columns)
+# reorder columns
+merged_data_no_na <- merged_data %>%
+  dplyr::select(FID, IID, everything()) %>% # Ensures FID and IID are the first two columns
+  drop_na()
+
+# replace FID = rel_family_id, IID = src_subject_id, remove PC11-PC20 and reported ethnicity cols
+merged_data_final <- merged_data_no_na %>%
+  mutate(FID = IID) %>%
+  dplyr::select(-PC11:-PC20, -ethnicity)
+
+# split merged_data_final by TOPMed ethnicity
+# Define the file names for each ethnicity
+afr_file <- file.path(anc_pc_dir, "ABCD.ancestry_knn.AFR.2263.txt")
+amr_file <- file.path(anc_pc_dir, "ABCD.ancestry_knn.AMR.2019.txt")
+eur_file <- file.path(anc_pc_dir, "ABCD.ancestry_knn.EUR.6891.txt")
+
+# Read the IIDs from each file
+afr_iids <- read.table(afr_file, header = FALSE, stringsAsFactors = FALSE)[, 1]
+amr_iids <- read.table(amr_file, header = FALSE, stringsAsFactors = FALSE)[, 1]
+eur_iids <- read.table(eur_file, header = FALSE, stringsAsFactors = FALSE)[, 1]
+
+merged_data_final$ethnicity <- NA
+
+# Assign ethnicity based on IIDs
+merged_data_final$ethnicity[merged_data_final$IID %in% afr_iids] <- "AFR"
+merged_data_final$ethnicity[merged_data_final$IID %in% amr_iids] <- "AMR"
+merged_data_final$ethnicity[merged_data_final$IID %in% eur_iids] <- "EUR"
+
+# Remove samples with missing ethnicity and reorder cols
+merged_data_final <- merged_data_final %>%
+  filter(!is.na(ethnicity)) %>%
+  relocate(batch, ethnicity, PC1:PC10, .after = interview_age)
+
+# Split pheno, covar and qcovar files by ethnicity and sex and then save
+# for (ethnicity in ethnicities) {
+#   for (sex in sexes) {
+#     # Filter data by ethnicity and sex
+#     df_subset <- filter_data(merged_data_final, ethnicity, sex)
+#     
+#     # Apply rank inverse normalization to phenotypes
+#     df_subset <- apply_rank_inverse_norm(df_subset)
+#     
+#     # Save GCTA files
+#     save_gcta_files(df_subset, ethnicity, sex, pheno_dir, covar_dir, date)
+#   }
+# }
+for (ethnicity in ethnicities) {
+  for (sex in sexes) {
+    save_split_data(merged_data_final, ethnicity, sex, pheno_dir, covar_dir, date)
+  }
+}
+
+
+#### Extra and QC ####
+# Subset the merged_data_final dataframe by ethnicity
+# afr_data <- merged_data_final[merged_data_final$IID %in% afr_iids, ]
+# amr_data <- merged_data_final[merged_data_final$IID %in% amr_iids, ]
+# eur_data <- merged_data_final[merged_data_final$IID %in% eur_iids, ]
+
+# Optional: Check the number of rows in each subset to ensure correctness
+# cat("Total number of individuals:", nrow(merged_data_final), "\n")
+# cat("Number of AFR individuals:", nrow(afr_data), "\n")
+# cat("Number of AMR individuals:", nrow(amr_data), "\n")
+# cat("Number of EUR individuals:", nrow(eur_data), "\n")
+# Create a new ethnicity column in merged_data_final
+
+# Save final_data as a space-separated text file suitable for GCTA MLMA
+# setwd(pheno_dir)
+# write.table(merged_data_final, file = "gcta_mlma_SCS_ROC_master.txt", sep = " ", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# first_100_rows <- head(merged_data_final, 100)
+# write.csv(first_100_rows, "first_100_rows.csv", row.names = FALSE)
+
+# Filtering rows with any `NA` values
+# rows_with_na <- merged_data %>% 
+#   filter(if_any(everything(), is.na)) %>%
+#   dplyr::select(FID, IID, batch, PC1:PC10)
+
+# Filter out rows in `rows_with_na` where `IID` is not present in either `ancestry_pcs$IID` or `covariate_data$IID`
+# This results in a dataset with `IID`s that are missing in either data frame
+# all_IIDs_missing <- rows_with_na %>%
+#   filter(!(IID %in% ancestry_pcs$IID) | !(IID %in% covariate_data$IID))
+
+# Extract unique IIDs and save to a text file
+# unique_IIDs <- all_IIDs_missing %>% 
+#   pull(IID) %>%
+#   unique()
+
+# Write IIDs to a text file
+# write_lines(unique_IIDs, "missing_IIDs.txt")
+
+# Filter `smri.R5.1.baseline.y2` to retain rows with `src_subject_id` present in `all_IIDs_missing$IID`
+# smri.R5.1.baseline.y2_missing_baseline_pcs <- smri.R5.1.baseline.y2 %>%
+#   filter(src_subject_id %in% all_IIDs_missing$IID) %>%
+#   distinct(src_subject_id, .keep_all = TRUE)
+
+# Save merged_data_no_na as a space-separated text file
+# write.table(smri.R5.1.baseline.y2_missing_baseline_pcs, file = "missing_IIDs_info.txt", sep = " ", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+# Covariate file
+# covariate_data <- final_data %>%
+#   dplyr::select(src_subject_id, sex, batch, mri_info_deviceserialnumber) %>%
+#   rename(IID = src_subject_id)
+
+# save covariate file
+# write.table(covariate_data, file = "covariate.txt", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+# Quantitative Covariate file
+
+# save phenotype files
+# Assuming gcta.pheno.scs.vol.roc is your data frame and pheno_dir is the directory where you want to save the files
+
+#### From EMMA/Sruthi ####
+# Read ancestry ID files
+read_ancestry_ids <- function(file_path) {
+  read.table(file_path, header = FALSE, stringsAsFactors = FALSE)$V2
+}
+
+ancestry_files <- list(
+  AFR = "/u/project/lhernand/sganesh/gwas_srs/TOPMed_imputed/ABCDr5_AFR.2263_no.sexmismatch_IDs.txt",
+  AMR = "/u/project/lhernand/sganesh/gwas_srs/TOPMed_imputed/ABCDr5_AMR.2019_no.sexmismatch_IDs.txt",
+  EUR = "/u/project/lhernand/sganesh/gwas_srs/TOPMed_imputed/ABCDr5_EUR.6891_no.sexmismatch_IDs.txt"
+)
+
+ancestry_samples_id_list <- map(ancestry_files, read_ancestry_ids)
+
+# Split phenotype data by ancestry
+split_by_ancestry <- function(pheno_data, ancestry_samples_id_list) {
+  map2(ancestry_samples_id_list, names(ancestry_samples_id_list), function(ids, ancestry_name) {
+    pheno_data %>% filter(IID %in% ids) %>% mutate(ancestry = ancestry_name)
+  })
+}
+
+# Split phenotype data by ancestry and gender
+split_by_ancestry_and_gender <- function(pheno_data_by_ancestry) {
+  gender <- c("M", "F")
+  pheno_data_by_ancestry_and_gender <- list()
+  
+  for (i in seq_along(pheno_data_by_ancestry)) {
+    pheno_data <- pheno_data_by_ancestry[[i]]
+    pheno_name <- tolower(names(pheno_data_by_ancestry[i]))
+    
+    for (j in seq_along(gender)) {
+      name <- paste(pheno_name, tolower(gender[j]), sep = "_")
+      pheno_data_by_ancestry_and_gender[[name]] <- pheno_data %>% filter(sex == gender[j])
+    }
+  }
+  
+  return(pheno_data_by_ancestry_and_gender)
+}
+
+# Save phenotype tables with ancestry info
+save_pheno_tables <- function(pheno_data_list, output_dir, date) {
+  setwd(output_dir)
+  
+  walk2(pheno_data_list, names(pheno_data_list), function(dt, pheno_name) {
+    n <- nrow(dt)
+    file_name <- paste0(pheno_name, "_1yr_followup_before_srs_within_ancestry_group_noNAs_", n, "_table_", date, ".csv")
+    write.csv(dt, file_name, row.names = TRUE)
+  })
+}
+
+# Main processing function
+process_phenotype_data <- function(pheno_data, ancestry_samples_id_list, output_dir, date) {
+  # Split by ancestry
+  pheno_by_ancestry <- split_by_ancestry(pheno_data, ancestry_samples_id_list)
+  
+  # Split by ancestry and gender
+  pheno_by_ancestry_and_gender <- split_by_ancestry_and_gender(pheno_by_ancestry)
+  
+  # Combine both lists
+  data <- c(pheno_by_ancestry, pheno_by_ancestry_and_gender)
+  
+  # Save tables
+  save_pheno_tables(data, output_dir, date)
+}
+
+# Usage
+pheno <- gcta.pheno.scs.vol.roc  # Your phenotype data
+process_phenotype_data(pheno, ancestry_samples_id_list, pheno_qc_dir, date)
+
---
+++ Analysis/Bak/release5_longit_sMRI_CB_DATA_CHAR.R
@@ -0,0 +1,197 @@
+#### LOAD PACKAGES AND ABCD 5.1 DATA, DEFINE DIRECTORIES, AND SOURCE EXTERNAL FUNCTIONS ####
+
+# packages
+library(pacman)
+p_load(dplyr, tidyr, ggplot2, rlang, lme4, plotrix, ggrepel, gt, ggridges, stringr, reshape2, plotly, shiny, shinybg, data.table)
+
+# directories
+base_dir <- '/u/project/lhernand/cobeaman/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis/'
+image_dir <- paste0(base_dir, 'Images/')
+shiny_dir <- paste0(base_dir, 'plots/Shinyapps_plotly_plots/')
+table1_dir <- paste0(shiny_dir, 'Counts.Table')
+table2_dir <- paste0(shiny_dir, 'ROC.Summary.Table')
+gwas_dir <- paste0(base_dir, 'GCTA_GWAS/')
+anc_pc_dir <- paste0(gwas_dir, 'ANCESTRY_PCS/')
+pheno_dir <- paste0(gwas_dir, 'PHENOTYPE_DATA')
+
+# ABCD 5.1 data and external functions
+# load(paste0(base_dir,'ABCD.Release.5.1.C4.Merged.Data.Rda'))
+abcdData.R5.1 <- readRDS(paste0(base_dir,'ABCD.Release.5.1.Merged.Data.ICV.rds'))
+source(paste0(base_dir, 'release5_external_functions.R'))
+
+#### RECODE SEX AND TIMEPOINT COLUMNS AND DEFINE SAMPLES AND TIMEPOINT FIELDS ####
+
+# recode sex and timepoint columns
+abcdData.R5.1 <- abcdData.R5.1 %>%
+  mutate(
+    timepoint = recode.timepoint(eventname),
+    sex = recode.sex(sex)
+  )
+
+# define samples and timepoint fields
+samples <- unique(abcdData.R5.1$src_subject_id)
+id.timepoint.fields <- c('src_subject_id', 'eventname', 'timepoint')
+
+#### SMRI FILTERING AND PREPARATION ####
+
+# filter for smri t1w qc metric and other relevant columns
+smri.R5.1 <- abcdData.R5.1 %>%
+  filter(imgincl_t1w_include == 1) %>%
+  dplyr::select(matches('smri|src_subject_id|eventname|timepoint|ethnicity|pc|ses|interview_age|sex|site_id_l|rel_family_id|mri_info_deviceserialnumber|mrisdp_453|pps')) %>%
+  filter(rowSums(is.na(.)) != ncol(.))
+
+# average across brain hemispheres for each applicable scs region
+smri.R5.1 <- average_hemispheres(smri.R5.1)
+
+# extract samples with smri data at just baseline and year 2 timepoints
+smri.R5.1.baseline.y2 <- smri.R5.1 %>%
+  filter(timepoint %in% c(0, 2)) %>%
+  group_by(src_subject_id) %>%
+  filter(all(c(0, 2) %in% timepoint)) %>%
+  ungroup()
+
+# extract samples with smri data at all timepoints
+smri.R5.1.all <- smri.R5.1 %>%
+  filter(timepoint %in% c(0, 2, 4)) %>%
+  group_by(src_subject_id) %>%
+  filter(all(c(0, 2, 4) %in% timepoint)) %>%
+  ungroup()
+
+#### DATA CHARACTERIZATION  ####
+
+# Define scs columns for volume measurements
+roc_volumes <- grep("smri_vol_scs_", names(smri.R5.1.baseline.y2), value = TRUE)
+
+# Calculate the percent rates of change for each scs ROI volume in smri.R5.1.baseline.y2 and smri.R5.1.all
+# remove sex == NA and only return values for sex, mri_info_deviceserialnumber, and interview_age from latest timepoint
+smri.R5.1.baseline.y2.ROC <- baseline_y2_roc(smri.R5.1.baseline.y2, roc_volumes)
+smri.R5.1.all.ROC <- all_timepoints_roc(smri.R5.1.all, roc_volumes)
+
+# convert smri.R5.1.all.ROC and smri.R5.1.all to ggplot2 long format
+smri.R5.1.all.ROC.long <- pivot_roc_to_long_format(smri.R5.1.all.ROC, is_baseline_y2 = FALSE)
+smri.R5.1.all.long <- pivot_original_to_long_format(smri.R5.1.all, roc_volumes)
+
+# convert smri.R5.1.baseline.y2.ROC and smri.R5.1.baseline.y2 to ggplot2 long format
+smri.R5.1.baseline.y2.ROC.long <- pivot_roc_to_long_format(smri.R5.1.baseline.y2.ROC, is_baseline_y2 = TRUE)
+smri.R5.1.baseline.y2.long <- pivot_original_to_long_format(smri.R5.1.baseline.y2, roc_volumes)
+
+#### PLOTTING ####
+
+# view sample size tables for smri.R5.1.all and smri.R5.1.baseline.y2 and run in background
+port <- httpuv::randomPort(); runBackgroundApp(appDir = table1_dir, port = port); Sys.sleep(1); view_app(port = port)
+# kill after viewing
+# shinybg::kill_all_apps()
+
+# view ROC summary tables for smri.R5.1.all.ROC.long and smri.R5.1.baseline.y2.ROC.long and run in background
+port <- httpuv::randomPort(); runBackgroundApp(appDir = table2_dir, port = port); Sys.sleep(1); view_app(port = port)
+# kill after viewing
+# shinybg::kill_all_apps()
+
+# create violin/box plots for annual rate of change for volume (toggle all timepoints or baseline y2)
+# violin plot of annual rate of change by region
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+            body { text-align: center; margin: auto; }
+            .shiny-input-container { margin: 20px auto; width: 50%; } /* Center and adjust width of dropdown */
+            .shiny-plot-output { margin: auto; width: 90%; } /* Adjust plot width and centering */
+            .js-plotly-plot .plotly .legend { font-size: 16px; } /* Increase legend font size */
+        "))
+  ),
+  titlePanel("Interactive Violin Plots of SCS ROI Rate of Change", windowTitle = "Rate of Change Analysis"),
+  selectInput("dataset", "Select Dataset:", choices = c("All Timepoints" = "all", "Baseline + Y2 Only" = "baseline")),
+  selectInput("roi_select", "Select ROI:", choices = NULL),  # Choices will be updated based on dataset
+  plotlyOutput("roi_plot", height = "600px")
+)
+# Define server logic
+server <- function(input, output, session) {
+  # Load data
+  all_data <- smri.R5.1.all.ROC.long
+  baseline_data <- smri.R5.1.baseline.y2.ROC.long
+  # Observe changes in dataset and update ROI selection choices
+  observe({
+    data <- if (input$dataset == "all") {
+      all_data
+    } else {
+      baseline_data
+    }
+    updateSelectInput(session, "roi_select", choices = unique(data$roi))
+  })
+  # Generate plot based on selected dataset and ROI
+  output$roi_plot <- renderPlotly({
+    data <- if (input$dataset == "all") {
+      all_data
+    } else {
+      baseline_data
+    }
+    # Ensure ROI is selected
+    req(input$roi_select)
+    filtered_data <- data[data$roi == input$roi_select, ]
+    plot_ly(data = filtered_data, type = "violin",
+            x = ~roi, y = ~Value,
+            color = ~roi, hoverinfo = 'text',
+            text = ~paste('ROI:', roi, '<br>Value:', Value)) %>%
+      layout(
+        title = paste("Distribution of Rate of Change for", input$roi_select),
+        yaxis = list(title = "Rate of Change (mm³/yr)", titlefont = list(size = 16), tickfont = list(size = 12), standoff = 20),
+        xaxis = list(title = "ROI", tickangle = 0, tickfont = list(size = 12)),
+        legend = list(title = "Time Comparison", orientation = "v", x = 1.05, y = 0.5, xanchor = 'left', yanchor = 'middle', font = list(size = 16)),
+        margin = list(l = 40, r = 40, t = 50, b = 50)
+      )
+  })
+}
+shinyApp(ui = ui, server = server)
+
+# box plot of region volumes
+
+# Calculate the correlation matrix for scs ROI volume ROCs
+# Generate melted correlatoin matrix
+melted_correlation <- reshape2::melt(cor(smri.R5.1.baseline.y2.ROC[, roc_volumes], use = "pairwise.complete.obs"))
+# Plot correlation matrix heatmap
+ggplot(melted_correlation, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1, 1), space = "Lab", 
+                       name="Correlation") +
+  theme_minimal() +
+  theme(axis.text.y = element_text(face = "bold"),
+        axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"),
+        axis.title = element_text(size = 14, face = "bold")) +
+  labs(x = element_blank(), y = element_blank(), 
+       title = "Subcortical ROC Correlation Matrix Heatmap") +
+  coord_fixed()
+ggsave(filename = paste0(image_dir, 'Subcortical_ROC_Correlation_Matrix_Heatmap.pdf'), width = 8, height = 6)
+
+# Generate pooled and sex-separated histograms for each scs ROI volume ROC
+# Loop through each volume to generate and save histograms
+for (roc_volume in roc_volumes) {
+  # Generate pooled histogram
+  Pooled.ROC <- ggplot(smri.R5.1.baseline.y2.ROC, aes_string(x = roc_volume)) +
+    geom_histogram(bins = 50, fill = "cornflowerblue", color = "black") +
+    ggtitle(paste0('Rate of Change of ', roc_volume, ' (Pooled)')) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5))
+  print(Pooled.ROC)
+  ggsave(filename = paste0(image_dir, roc_volume, '_Pooled.pdf'), plot = Pooled.ROC, width = 8, height = 6)
+  # Generate sex-separated histograms
+  Sex.ROC <- ggplot(smri.R5.1.baseline.y2.ROC, aes_string(x = roc_volume)) +
+    geom_histogram(bins = 50, fill = "salmon", color = "black") +
+    facet_wrap(~sex) +
+    ggtitle(paste0('Rate of Change of ', roc_volume, ' (By Sex)')) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(filename = paste0(image_dir, roc_volume, '_BySex.pdf'), plot = Sex.ROC, width = 8, height = 6)
+}
+
+# Check the normality of these distributions
+for (roc_volume in roc_volumes) {
+  print(paste0("Shapiro-Wilk Test for ", roc_volume, " (Pooled):"))
+  print(shapiro.test(smri.R5.1.baseline.y2.ROC[[roc_volume]]))
+  
+  print(paste0("Shapiro-Wilk Test for ", roc_volume, " (Male):"))
+  print(shapiro.test(smri.R5.1.baseline.y2.ROC %>% filter(sex == "M")[[roc_volume]]))
+  
+  print(paste0("Shapiro-Wilk Test for ", roc_volume, " (Female):"))
+  print(shapiro.test(smri.R5.1.baseline.y2.ROC %>% filter(sex == "F")[[roc_volume]]))
+}
+
