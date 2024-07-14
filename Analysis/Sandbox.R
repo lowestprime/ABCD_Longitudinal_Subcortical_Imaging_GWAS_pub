fb9b850f712d42586a73f1ebc3e4eb3f37100c20
lowestprime <cooperbeaman@gmail.com>
2024-07-14 13:48:24 -0700

    feat: Split merged_data_final by TOPMed ethnicity and create dummy variables

--- Analysis/Sandbox.R
+++ Analysis/Sandbox.R
@@ -10,7 +10,11 @@ pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
 pacman::p_loaded()
 
 # count number of each unique in dataframe column
-device_counts <- summary(factor(merged_data_final$mri_info_deviceserialnumber))
+summary(factor(merged_data_final$mri_info_deviceserialnumber))
+summary(factor(merged_data_final$batch))
+summary(factor(c(merged_data_final$mri_info_deviceserialnumber, merged_data_final$batch)))
+combined_summary_df <- as.data.frame(as.table(summary(factor(c(merged_data_final$mri_info_deviceserialnumber, merged_data_final$batch)))))
+
 
 # take me back home
 setwd('~/')
@@ -22,7 +26,7 @@ setwd('~/project-lhernand/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis')
 paste(names(merged_data_final), collapse = ", ")
 
 # List all subdirectories
-list.dirs(path = 'GCTA_GWAS/Processed_Data', full.names = T, recursive = T)
+list.dirs(path = '~/project-lhernand/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis/GCTA_GWAS/Processed_Data', full.names = T, recursive = T)
 
 # sum different values in df
 sum(merged_data_no_na$ethnicity %in% c("AFR", "AMR", "EUR"))
