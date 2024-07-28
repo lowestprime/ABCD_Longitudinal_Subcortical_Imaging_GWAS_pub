f39d021299036da794e14391ddbe235b9623ba20
lowestprime <cooperbeaman@gmail.com>
2024-07-27 21:23:49 -0700

    Completed GCTA mlma test job script for European M whole brain volume ROC phenotype and added infile and grm paths to PATHS.txt

--- Analysis/GCTA_GWAS/Job_Scripts/gcta_mlma_subc_roc_gwas_EURm_whbr.sh
+++ Analysis/GCTA_GWAS/Job_Scripts/gcta_mlma_subc_roc_gwas_EURm_whbr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Perform Genome-Wide association analysis using GCTA
+# Perform Genome-Wide association analysis using GCTA - Test Run - EUR M smri_vol_scs_wholeb_ROC0_2
 
 # Set working directory
 #$ -wd /u/project/lhernand/cobeaman/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis/GCTA_GWAS/Processed_Data
@@ -10,73 +10,80 @@
 #$ -l arch=intel-gold*  # Requesting nodes with Intel Gold CPUs
 
 # Output and error handling
-#$ -o GCTA_GWAS_EURM_WhBr${JOB_ID}.log
+#$ -o GCTA_GWAS_EURM_WhBr${JOB_ID}.out
 #$ -j y # join std error and std output streams, yes
 
 # Email notifications
 #$ -M cobeaman@g.ucla.edu
 #$ -m bea # email when job begins, ends, and if it aborts
 
+# Define constants
+# Current date
+date=$(date +"%m%d%Y")
+# Population, sex, and phenotype
+pop="EUR"
+sex="M"
+phenotype="smri_vol_scs_wholeb_ROC0_2"
+
 # Directories
 base_dir="/u/project/lhernand/cobeaman/ABCD_Longitudinal_Subcortical_Imaging_GWAS/Analysis/GCTA_GWAS/Processed_Data"
-grmDir="/u/project/lhernand/cobeaman/grm/grm_all"
+indir="/u/project/lhernand/shared/GenomicDatasets-processed/ABCD_Release_5/genotype/TOPMed_imputed/splitted_by_ancestry_groups/males"
+grmDir="/u/project/lhernand/shared/GenomicDatasets-processed/ABCD_Release_5/genotype/GRM/grm_males"
 results_dir="${base_dir}/Results/test_run"
 mkdir -p "${results_dir}"
 
 # Software path
 gcta="/u/project/lhernand/sganesh/apps/gcta/gcta-1.94.1"
 
-# Date
-date=$(date +%Y%m%d)
+# Phenotype and covariate files
+# Find phenotype file
+pheno_file=$(find "${base_dir}/Phenotypes/${pop}/${sex}/" -type f -name "*${phenotype}*.txt")
 
-# Population, Sex, and Phenotype
-pop="EUR"
-sex="M"
-pheno_file="${base_dir}/Phenotypes/EUR/M/988282_pheno_07152024_EUR_M_2268_smri_vol_scs_wholeb_ROC0_2.txt"
+# Find covar file
+covar_file=$(find "${base_dir}/Covariates/Discrete/${pop}/${sex}/" -type f -name "covar_*${pop}_${sex}*.txt")
 
-# Covariate and Quantitative Covariate Files
-covar_file="${base_dir}/Covariates/Discrete/EUR/M/covar_988282_07152024_EUR_M_2268.txt"
-qcovar_file="${base_dir}/Covariates/Quantitative/EUR/M/qcovar_noICV_988282_07152024_EUR_M_2268.txt"
+# Determine qcovar prefix
+if [ "$phenotype" = "smri_vol_scs_wholeb_ROC0_2" ]; then
+  qcovar_prefix="qcovar_noICV_"
+else
+  qcovar_prefix="qcovar_"
+fi
+# Find qcovar file
+qcovar_file=$(find "${base_dir}/Covariates/Quantitative/${pop}/${sex}/" -type f -name "${qcovar_prefix}*${pop}_${sex}*.txt")
 
 # GRM and bfile
-num_no_sex_mismatch=$(ls -1v $grmDir/*${pop}*.rmsexmismatch*.fam | grep -v "allproblem" | wc -l)
-grm_file_prefix="${grmDir}/ABCD_202209.updated.nodups.curated.cleaned_indivs.qc.basic_withsexinfo_RSid_NoDuplicates_RSidOnly_${pop}.${num_no_sex_mismatch}.rmsexmismatch.0.chr1.22_indep_pruned"
+infile="${indir}/${pop}.males.genotype"
+grm_file="${grmDir}/${pop}.males_GRM"
 
 # Output file
-pheno_name=$(basename "${pheno_file}" .txt)
 num_samples=$(wc -l < "${pheno_file}")
-out_file="${results_dir}/${pheno_name}_${date}_n${num_samples}"
+out_file="${results_dir}/${phenotype}_${date}_n${num_samples}"
 
 # Check if all necessary files exist
-if [ ! -f "${pheno_file}" ]; then
-  echo "Phenotype file ${pheno_file} does not exist. Exiting."
-  exit 1
-fi
+required_files=(
+    "${pheno_file}"
+    "${covar_file}"
+    "${qcovar_file}"
+    "${grm_file}.grm.bin" "${grm_file}.grm.id" "${grm_file}.grm.N.bin"
+    "${infile}.bed" "${infile}.bim" "${infile}.fam"
+)
 
-if [ ! -f "${covar_file}" ]; then
-  echo "Covariate file ${covar_file} does not exist. Exiting."
-  exit 1
-fi
+# Loop through each required file and check if it exists
+for file in "${required_files[@]}"; do
+    if [ ! -f "$file" ]; then
+        echo "Required file $file does not exist. Exiting."
+        exit 1
+    else
+        echo "Found required file: $file"
+    fi
+done
 
-if [ ! -f "${qcovar_file}" ]; then
-  echo "Quantitative covariate file ${qcovar_file} does not exist. Exiting."
-  exit 1
-fi
-
-if [ ! -f "${grm_file_prefix}.bed" ] || [ ! -f "${grm_file_prefix}.bim" ] || [ ! -f "${grm_file_prefix}.fam" ]; then
-  echo "One or more of the GRM binary files (${grm_file_prefix}.bed, .bim, .fam) do not exist. Exiting."
-  exit 1
-fi
-
-if [ ! -f "${grm_file_prefix}_qc_GRM.grm.bin" ] || [ ! -f "${grm_file_prefix}_qc_GRM.grm.id" ] || [ ! -f "${grm_file_prefix}_qc_GRM.grm.N.bin" ]; then
-  echo "One or more of the GRM files (${grm_file_prefix}_qc_GRM.grm.bin, .grm.id, .grm.N.bin) do not exist. Exiting."
-  exit 1
-fi
+echo "All required files found. Starting GCTA --mlma analysis."
 
 # Run GCTA MLMA
 $gcta --mlma \
-      --bfile "${grm_file_prefix}" \
-      --grm "${grm_file_prefix}_qc_GRM" \
+      --bfile "${infile}" \
+      --grm "${grm_file}" \
       --pheno "${pheno_file}" \
       --covar "${covar_file}" \
       --qcovar "${qcovar_file}" \
@@ -84,8 +91,8 @@ $gcta --mlma \
       --out "${out_file}"
 
 if [ $? -ne 0 ]; then
-  echo "Error running GCTA MLMA for ${pheno_name}" >&2
+  echo "Error running GCTA MLMA for ${phenotype}" >&2
   exit 1
 fi
 
-echo "Completed GCTA MLMA for ${pheno_name} with ${num_samples} samples"
+echo "Completed GCTA MLMA for ${pop} ${sex} ${phenotype} with ${num_samples} samples"
